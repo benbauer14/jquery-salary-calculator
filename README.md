cdd9978e6a6a21f3a93d0170520c7f674e07b084
Ben Bauer <ben.bauer1@gmail.com>
2021-03-06 13:43:27 -0600

    stretch goals complete. styling complete. wduplicate entries not allowed. warning displays if box input is empty or duplicate names entered

--- README.md
+++ README.md
@@ -28,9 +28,9 @@ No files have been provided (just instructions.md and a readme.md). Instead of f
 
 ## Stretch Mode
 
-[] Add styling or extra functionality that fits with the theme of this assignment.
+[x] Add styling or extra functionality that fits with the theme of this assignment.
 
-[] Once the employee is deleted, update the total spend on salaries account for this employee's removal. This will require that the logic knows which element was removed. You will need to use `.text()` as a getter or look into jQuery's `.data()` function. This is tricky! 
+[x] Once the employee is deleted, update the total spend on salaries account for this employee's removal. This will require that the logic knows which element was removed. You will need to use `.text()` as a getter or look into jQuery's `.data()` function. This is tricky! 
 
 ## Reminder About Modes
 
--- scripts/client.js
+++ scripts/client.js
@@ -9,13 +9,54 @@ function readyNow(){
   } 
 
   function addEmployee (){
-        //###############Remove ability to add duplicates
-        //###############Remove ability to add duplicates
-                //###############Remove ability to add duplicates
-                        //###############Remove ability to add duplicates
-                                //###############Remove ability to add duplicates
-                                        //###############Remove ability to add duplicates
-    //add employee to Employees to array with delete button
+        //clear warning
+        $('#warning').text("")
+        //check for empty inputs if so flash the empty box at the user
+        if($('#firstNameIn').val() === ""){
+          $('#firstNameIn').fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150)
+          $('#warning').text("Please enter a first name!")
+          return null
+        }
+        if($('#lastNameIn').val() === ""){
+          $('#lastNameIn').fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150)
+          $('#warning').text("Please enter a last name!")
+          return null
+        }
+        if($('#IDIn').val() === ""){
+          $('#IDIn').fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150)
+          $('#warning').text("Please enter an ID!")
+          return null
+        }
+        if($('#titleIn').val() === ""){
+          $('#titleIn').fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150)
+          $('#warning').text("Please enter a title!")
+          return null
+        }
+        if($('#salaryIn').val() === ""){
+          $('#salaryIn').fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150)
+          $('#warning').text("Please enter a salary!")
+          return null
+        }
+        //check for empty inputs
+        //flash input if empty
+        //check if duplicate entry
+        //combine all inputs into a single string
+        let newEntry = $('#firstNameIn').val() + $('#lastNameIn').val() + $('#IDIn').val() + $('#titleIn').val() + $('#salaryIn').val()
+        //loop through array and combine each index into a single string
+        for( let i = 0; i < employees.length; i++){
+          let combinedIndex = employees[i].firstName + employees[i].lastName + employees[i].ID + employees[i].title+ employees[i].salary
+          if(combinedIndex === newEntry){
+            console.log(combinedIndex)
+            console.log(newEntry)
+            //provide warning that employee exists
+            $('#warning').text("Employee already exists!")
+            //if input matches an existing Employee return ""
+            return null;
+          }
+        }
+
+
+    //add employee to Employees to array and create the delete button
     employees.push({
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
@@ -24,7 +65,7 @@ function readyNow(){
         salary: $('#salaryIn').val()
     })
     $('#employeeTable').empty() //empty employee table
-    $('#employeeTable').append(`<tr><td>First Name</td><td>Last Name</td><td>ID</td><td>Title</td><td>Annual Salary</td><td>Action</td></tr>`)
+    $('#employeeTable').append(`<tr><th>First Name</th><th>Last Name</th><th>ID</th><th>Title</th><th>Annual Salary</th><th>Action</th></tr>`)
     for(let i = 0; i < employees.length; i++){
         $('#employeeTable').append(`<tr><td>${employees[i].firstName}</td><td>${employees[i].lastName}</td><td>${employees[i].ID}</td><td>${employees[i].title}</td><td>${employees[i].salary}</td><td><button class="deleteEmployeeButton">Delete</button></td></tr>`)
     }
@@ -40,14 +81,19 @@ function readyNow(){
       for(let i = 0; i < employees.length; i++){
           //using a loop combine all text from object values into a single string with the word delete appended to the end
           let combinedText = employees[i].firstName + employees[i].lastName + employees[i].ID + employees[i].title+ employees[i].salary+"Delete"
-          console.log(combinedText);
+
           //compare the created string to the parent parent text 
-          //if they match remove the index from the table
+          if(combinedText = $(this).parent().parent().text()){
+            //if they match remove the index from the table
+              employees.splice(i, 1)
+            //remove row from table
+            $(this).parent().parent().empty()
+          }
+
           //run monthlyCost
+          monthlyCost()
       }
-      console.log($(this).parent().parent().text())
-      //remove row from table
-      $(this).parent().parent().empty()
+
   }
 
   function monthlyCost(){
@@ -55,7 +101,7 @@ function readyNow(){
       for(let i = 0; i < employees.length; i++){
           total += Number(employees[i].salary)/12
       }
-      $("#monthlyCost").text(total)
+      $("#monthlyCost").text(Math.round(total))
       if(total > 20000){
         $("#totalMonthly").css({"background-color" : "red"})
       }else{
